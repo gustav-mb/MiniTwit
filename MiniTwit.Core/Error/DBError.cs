9c5b4f9551592915dd07e6fe70a87c732031ee8a
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-05-29 14:08:37 +0200

    feat: Added new errors

--- MiniTwit.Core/Error/DBError.cs
+++ MiniTwit.Core/Error/DBError.cs
@@ -6,7 +6,10 @@ public enum DBError
 {
     INVALID_USER_ID,
     INVALID_USERNAME,
-    INVALID_PASSWORD
+    INVALID_PASSWORD,
+    FOLLOW_SELF,
+    UNFOLLOW_SELF,
+    USERNAME_TAKEN
 }
 
 public static class DBErrorExtensions
@@ -16,6 +19,9 @@ public static class DBErrorExtensions
         INVALID_USER_ID => "Invalid user id",
         INVALID_USERNAME => "Invalid username",
         INVALID_PASSWORD => "Invalid password",
+        FOLLOW_SELF => "Can't follow yourself",
+        UNFOLLOW_SELF => "Can't unfollow yourself",
+        USERNAME_TAKEN => "Username is already taken",
         _ => throw new NotSupportedException($"DBError of type '{typeof(DBError)}' not supported!")
     };
 }
