055d506c2d577f97fd9f17970cf415888bf77c0e
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-05-29 21:19:43 +0200

    fix: Added missing response code

--- MiniTwit.Server/Controllers/UserController.cs
+++ MiniTwit.Server/Controllers/UserController.cs
@@ -29,6 +29,7 @@ public class UserController : ControllerBase
 
     [HttpPost("/register")]
     [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<ActionResult> Register([FromBody] UserCreateDTO userCreateDTO)
     {
