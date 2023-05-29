d393f30369315fe9f796efad575270d08765e1b2
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-05-29 20:10:50 +0200

    refactor: Changed limit to nullable

--- MiniTwit.Service/IServices/ITweetService.cs
+++ MiniTwit.Service/IServices/ITweetService.cs
@@ -6,7 +6,7 @@ namespace MiniTwit.Service.IServices;
 public interface ITweetService
 {
     Task<APIResponse> CreateTweetAsync(TweetCreateDTO tweetCreateDTO);
-    Task<APIResponse<IEnumerable<TweetDTO>>> GetUsersTweetsAsync(string username, int limit = 30, CancellationToken ct = default);
-    Task<APIResponse<IEnumerable<TweetDTO>>> GetAllNonFlaggedTweetsAsync(int limit = 30, CancellationToken ct = default);
-    Task<APIResponse<IEnumerable<TweetDTO>>> GetUsersAndFollowedNonFlaggedTweetsAsync(string userId, int limit = 30, CancellationToken ct = default);
+    Task<APIResponse<IEnumerable<TweetDTO>>> GetUsersTweetsAsync(string username, int? limit = null, CancellationToken ct = default);
+    Task<APIResponse<IEnumerable<TweetDTO>>> GetAllNonFlaggedTweetsAsync(int? limit = null, CancellationToken ct = default);
+    Task<APIResponse<IEnumerable<TweetDTO>>> GetUsersAndFollowedNonFlaggedTweetsAsync(string userId, int? limit = null, CancellationToken ct = default);
 }
