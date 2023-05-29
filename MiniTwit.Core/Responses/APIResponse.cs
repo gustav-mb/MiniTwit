e78f9b1a6a7bc4d4d9e75f1abc6f599ba75c1eed
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-05-29 21:19:13 +0200

    feat: Added overloaded contructors

--- MiniTwit.Core/Responses/APIResponse.cs
+++ MiniTwit.Core/Responses/APIResponse.cs
@@ -7,7 +7,15 @@ namespace MiniTwit.Core.Responses;
 public record APIResponse
 {
     public HTTPResponse HTTPResponse { get; init; }
-    public APIError? Error { get; init; }
+    public APIError? APIError { get; init; }
+
+    public APIResponse(HTTPResponse httpResponse, DBError? dbError)
+    {
+        HTTPResponse = httpResponse;
+        APIError = dbError.ToAPIError(httpResponse);
+    }
+
+    public APIResponse(HTTPResponse httpResponse) : this(httpResponse, null) { }
 
     public ActionResult ToActionResult() => CreateActionResult();
 
@@ -16,10 +24,10 @@ public record APIResponse
         Ok => new OkObjectResult(model),
         Created => new CreatedResult(location, model),
         NoContent => new NoContentResult(),
-        BadRequest => new BadRequestObjectResult(Error),
-        Unauthorized => new UnauthorizedObjectResult(Error),
-        NotFound => new NotFoundObjectResult(Error),
-        Conflict => new ConflictObjectResult(Error),
+        BadRequest => new BadRequestObjectResult(APIError),
+        Unauthorized => new UnauthorizedObjectResult(APIError),
+        NotFound => new NotFoundObjectResult(APIError),
+        Conflict => new ConflictObjectResult(APIError),
         _ => throw new NotSupportedException($"HTTPResponse {HTTPResponse} not supported!")
     };
 }
@@ -27,6 +35,13 @@ public record APIResponse
 public record APIResponse<T> : APIResponse
 {
     public T? Model { get; init; }
+
+    public APIResponse(HTTPResponse httpResponse, T? model, DBError? dbError) : base(httpResponse, dbError)
+    {
+        Model = model;
+    }
+
+    public APIResponse(HTTPResponse httpResponse, T? model) : this(httpResponse, model, null) { }
 
     public ActionResult<T> ToActionResult(string location) => base.CreateActionResult(Model, location);
     public new ActionResult<T> ToActionResult() => base.CreateActionResult(Model);
