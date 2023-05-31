403ce18bbbd5ec38133cd1c887bcaee3ce2d0393
Gustav Metnik-Beck <90196187+gustav-mb@users.noreply.github.com>
2023-05-31 11:34:14 +0200

    feat: Implemented ServiceManager (#1)

--- MiniTwit.Service/ServiceManager.cs
+++ MiniTwit.Service/ServiceManager.cs
@@ -1,6 +1,24 @@
+using MiniTwit.Core.IRepositories;
+using MiniTwit.Security.Hashing;
+using MiniTwit.Service.IServices;
+using MiniTwit.Service.Services;
+
 namespace MiniTwit.Service;
 
-public class ServiceManager
+public class ServiceManager : IServiceManager
 {
-    //
+    private readonly Lazy<IUserService> _lazyUserService;
+    private readonly Lazy<IFollowerService> _lazyFollowerService;
+    private readonly Lazy<ITweetService> _lazyTweetService;
+
+    public IUserService UserService => _lazyUserService.Value;
+    public IFollowerService FollowerService => _lazyFollowerService.Value;
+    public ITweetService TweetService => _lazyTweetService.Value;
+
+    public ServiceManager(IUserRepository userRepository, IFollowerRepository followerRepository, ITweetRepository tweetRepository, IHasher hasher)
+    {
+        _lazyUserService = new Lazy<IUserService>(() => new UserService(userRepository, hasher));
+        _lazyFollowerService = new Lazy<IFollowerService>(() => new FollowerService(followerRepository));
+        _lazyTweetService = new Lazy<ITweetService>(() => new TweetService(tweetRepository));
+    }
 }
