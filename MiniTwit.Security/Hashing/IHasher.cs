e06d88fe1ab6edb2470dcf96f0b702fdadc2cf85
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-01 21:26:42 +0200

    feat: Implemented non async hash methods

--- MiniTwit.Security/Hashing/IHasher.cs
+++ MiniTwit.Security/Hashing/IHasher.cs
@@ -2,6 +2,8 @@ namespace MiniTwit.Security.Hashing;
 
 public interface IHasher
 {
+    (string Hash, string Salt) Hash(string data);
     Task<(string Hash, string Salt)> HashAsync(string data);
+    bool VerifyHash(string data, string hash, string salt);
     Task<bool> VerifyHashAsync(string data, string hash, string salt);
 }
