6c1c78c5967d2fc7d5d7068afbbed371fdbe040d
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-01 08:16:15 +0200

    fix: Changed to AuthorId

--- MiniTwit.Core/DTOs/TweetDTO.cs
+++ MiniTwit.Core/DTOs/TweetDTO.cs
@@ -2,7 +2,7 @@ namespace MiniTwit.Core.DTOs;
 
 public record TweetDTO
 {
-    public string AuthorName { get; init; } = null!;
+    public string AuthorId { get; init; } = null!;
     public string Text { get; init; } = null!;
     public DateTime PubDate { get; init; }
 }
