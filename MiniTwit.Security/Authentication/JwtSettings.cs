b4d70e828867ce6bff678b598b17ebbf2f42e22d
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-02 15:38:45 +0200

    feat: Implemented JwtSettings

---
+++ MiniTwit.Security/Authentication/JwtSettings.cs
@@ -0,0 +1,8 @@
+namespace MiniTwit.Security.Authentication;
+
+public class JwtSettings
+{
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string Key { get; set; } = null!;
+}
