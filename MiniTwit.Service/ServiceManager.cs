72b608f6c35e2359077372ccc9e02c2e26d94b04
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-08 10:08:46 +0200

    feat: Implemented AuthenticationService

--- MiniTwit.Service/ServiceManager.cs
+++ MiniTwit.Service/ServiceManager.cs
@@ -1,5 +1,6 @@
 using MiniTwit.Core.IRepositories;
 using MiniTwit.Security.Hashing;
+using MiniTwit.Security.Authentication.TokenGenerators;
 using MiniTwit.Service.IServices;
 using MiniTwit.Service.Services;
 
@@ -10,15 +11,18 @@ public class ServiceManager : IServiceManager
     private readonly Lazy<IUserService> _lazyUserService;
     private readonly Lazy<IFollowerService> _lazyFollowerService;
     private readonly Lazy<ITweetService> _lazyTweetService;
+    private readonly Lazy<AuthenticationService> _lazyAuthenticationService;
 
     public IUserService UserService => _lazyUserService.Value;
     public IFollowerService FollowerService => _lazyFollowerService.Value;
     public ITweetService TweetService => _lazyTweetService.Value;
+    public IAuthenticationService AuthenticationService => _lazyAuthenticationService.Value;
 
-    public ServiceManager(IUserRepository userRepository, IFollowerRepository followerRepository, ITweetRepository tweetRepository, IHasher hasher)
+    public ServiceManager(IUserRepository userRepository, IFollowerRepository followerRepository, ITweetRepository tweetRepository, ITokenRepository tokenRepository, IHasher hasher, ITokenGenerator tokenGenerator)
     {
         _lazyUserService = new Lazy<IUserService>(() => new UserService(userRepository, hasher));
         _lazyFollowerService = new Lazy<IFollowerService>(() => new FollowerService(followerRepository));
         _lazyTweetService = new Lazy<ITweetService>(() => new TweetService(tweetRepository));
+        _lazyAuthenticationService = new Lazy<AuthenticationService>(() => new AuthenticationService(userRepository, tokenRepository, hasher, tokenGenerator));
     }
 }
