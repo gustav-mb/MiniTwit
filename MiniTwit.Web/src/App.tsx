eb35b8bb965946ad11736913f7011a9be43003e9
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-22 08:35:14 +0200

    setup: Setup react project

---
+++ MiniTwit.Web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="page">
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
