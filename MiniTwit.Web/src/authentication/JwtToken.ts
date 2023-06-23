a505d385a9318dc5deafd81d92edf6620e966a08
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-23 22:01:44 +0200

    feat: Can now decode JWT access token for user claims

--- MiniTwit.Web/src/authentication/JwtToken.ts
+++ MiniTwit.Web/src/authentication/JwtToken.ts
@@ -1,7 +1,16 @@
+import jwtDecode from "jwt-decode"
+
 export function getUsername(): string {
     return ""
 }
 
 export function isLoggedIn(): boolean {
     return true
+}
+
+export function parseToken(token: string) {
+    const decodedToken = jwtDecode<any>(token)
+    console.log(decodedToken.nameid)
+    console.log(decodedToken.unique_name)
+    console.log(decodedToken.email)
 }
