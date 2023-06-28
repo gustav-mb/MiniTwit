ee90c3b3a2b45e093c95998b32723515ea535326
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-28 14:40:37 +0200

    chor: Accepts undefined strings

--- MiniTwit.Web/src/services/FollowerService.ts
+++ MiniTwit.Web/src/services/FollowerService.ts
@@ -3,11 +3,11 @@ import axiosClient from "../utilities/Axios"
 const ROUTE = 'Follower'
 
 class FollowerService {
-    public async followUser(username: string, userId: string): Promise<undefined> {
+    public async followUser(username: string | undefined, userId: string): Promise<undefined> {
         return await axiosClient.post(`${ROUTE}/${username}/follow?userId=${userId}`)
     }
 
-    public async unfollowUser(username: string, userId: string): Promise<undefined> {
+    public async unfollowUser(username: string | undefined, userId: string): Promise<undefined> {
         return await axiosClient.delete(`${ROUTE}/${username}/unfollow?userId=${userId}`)
     }
 }
