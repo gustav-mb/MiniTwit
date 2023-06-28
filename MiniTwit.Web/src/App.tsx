7e21dba25f99dc28eda44efdd200ba85c5dd30eb
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-28 14:41:01 +0200

    refactor: Moved main layout to Layout.tsx page

--- MiniTwit.Web/src/App.tsx
+++ MiniTwit.Web/src/App.tsx
@@ -1,35 +1,13 @@
 import './App.css';
-import './style.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Timeline from './pages/TimeLine';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import UserTimeline from './components/timelines/UserTimeline';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import { isLoggedIn } from './authentication/JwtToken';
-import PublicTimeline from './components/timelines/PublicTimeline';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './pages/Layout';
 
 function App() {
-  // useDocumentTitle("Welcome | MiniTwit")
-
   return (
     <>
-      <div className="page">
-        <Header isLoggedIn={isLoggedIn()}></Header>
-        <div className="body">
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Timeline></Timeline>}></Route>
-              <Route path="/public" element={<PublicTimeline></PublicTimeline>}></Route>
-              <Route path="/login" element={<Login></Login>}></Route>
-              <Route path="/register" element={<Register></Register>}></Route>
-              <Route path="/:username" element={<UserTimeline></UserTimeline>}></Route>
-            </Routes>
-          </BrowserRouter>
-        </div>
-        <Footer></Footer>
-      </div>
+        <BrowserRouter>
+          <Layout></Layout>
+        </BrowserRouter>
     </>
   );
 }
