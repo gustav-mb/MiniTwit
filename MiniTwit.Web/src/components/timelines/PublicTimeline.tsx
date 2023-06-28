4a292e5984490fcc75da2b2bb124faac8522a7f3
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-28 14:32:16 +0200

    feat: Implemented PublicTimeline component

--- MiniTwit.Web/src/components/timelines/PublicTimeline.tsx
+++ MiniTwit.Web/src/components/timelines/PublicTimeline.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
 import { TweetDTO } from "../../models/TweetDTO";
 import { useDocumentTitle } from "../../utilities/Utilities";
 import TweetCollection from "../tweet/TweetCollection";
+import TweetService from "../../services/TweetService";
+
+const tweetService = new TweetService()
 
 function PublicTimeline() {
     useDocumentTitle("Public Timeline")
-    const tweets: TweetDTO[] = []
+
+    const [tweets, setTweets] = useState<TweetDTO[]>([])
+
+    useEffect(() => {
+        const fetchTweets = async () => {
+            await tweetService.getPublicTimeline()
+                .then(tweets => setTweets(tweets))
+        }
+        
+        fetchTweets()
+    }, [])
 
     return (
         <>
