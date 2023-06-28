7529d155aed941b1ab0ed171947850a115f73bbb
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-28 14:39:50 +0200

    feat: Implemented response intercepter for success and error handling

--- MiniTwit.Web/src/utilities/Axios.ts
+++ MiniTwit.Web/src/utilities/Axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { APIError } from "../models/APIError";
 
 const PRODUCTION_BASE_URL = "http://127.0.0.1:5000/"
 const DEVELOPMENT_BASE_URL = "http://127.0.0.1:5000/"
@@ -9,6 +10,21 @@ const axiosClient = axios.create({
     headers: {
         'Content-Type': 'application/json'
     }
+})
+
+axiosClient.interceptors.response.use(response => {
+    return response.data
+},
+(error: Error) => {
+    if (axios.isAxiosError(error))
+        return Promise.reject(error.response?.data)
+
+    const APIError: APIError = {
+        errorMsg: error.message,
+        status: -1
+    } 
+
+    return Promise.reject(APIError)
 })
 
 export default axiosClient
