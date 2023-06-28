78750ca378008cd87b2ec37cd9651b762a2f48c3
Gustav Metnik-Beck <gustav.mb@outlook.com>
2023-06-28 14:32:39 +0200

    feat: Implemented basic MyTimeline component

--- MiniTwit.Web/src/components/timelines/MyTimeLine.tsx
+++ MiniTwit.Web/src/components/timelines/MyTimeLine.tsx
@@ -1,19 +1,49 @@
-import { getUsername } from "../../authentication/JwtToken";
+import { FormEvent, useEffect, useState } from "react";
+import { getUserId, getUsername } from "../../authentication/JwtToken";
+import TweetService from "../../services/TweetService";
+import { TweetDTO } from "../../models/TweetDTO";
+import TweetCollection from "../tweet/TweetCollection";
 import { useDocumentTitle } from "../../utilities/Utilities";
 
-function MyTimeline() {
+const tweetService = new TweetService()
+
+function MyTimeline({ setFlash }: { setFlash: (message: string) => void }) {
     useDocumentTitle("My Timeline")
 
+    const [tweets, setTweets] = useState<TweetDTO[]>([])
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        const fetchTweets = async () => {
+            await tweetService.getTimeline(getUserId())
+                .then(tweets => setTweets(tweets))
+                .catch(_ => true)
+        }
+        
+        fetchTweets()
+    }, [])
+
+    async function submit(e: FormEvent) {
+        e.preventDefault() // Prevent page from reloading on submit
+        
+        const userId = getUserId()
+
+        await tweetService.createTweet(userId, text)
+            .then(() => setFlash("Your message was recorded"))
+            .catch(() => true)
+    }
+
     return (
-         <>
+        <>
             <h2>My Timeline</h2>
             <div className="twitbox">
-                <h3>What's on your mind { getUsername() }?</h3>
-                <form>
-                    <input type="text" name="text" size={60}></input>
+                <h3>What's on your mind {getUsername()}?</h3>
+                <form onSubmit={submit}>
+                    <input type="text" name="text" size={60} onChange={e => setText(e.target.value)}></input>
                     <input type="submit" value="Share"></input>
                 </form>
             </div>
+            <TweetCollection tweets={tweets}></TweetCollection>
         </>
     );
 }
